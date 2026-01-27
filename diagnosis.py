9bcb400ef23ba79c20dfc8542c07e834f92b55b5
sanjaraT <sanjaratunola@gmail.com>
2026-01-27 12:40:52 +0600

    View Specific Record

--- diagnosis.py
+++ diagnosis.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 
 app = FastAPI()
@@ -20,3 +20,12 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id : str = Path(...,description='Enter your patient Id',example='P001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'error':'Patient Not Found'}
+
