2639c860a59f1cef80e7e05f871ddc0a7d669e0b
sanjaraT <sanjaratunola@gmail.com>
2026-01-27 13:41:00 +0600

    Query - Sorting record

--- diagnosis.py
+++ diagnosis.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Path,HTTPException
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 
 app = FastAPI()
@@ -28,4 +28,22 @@ def view_patient(patient_id : str = Path(...,description='Enter your patient Id'
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code=404, detail='Patient Not Found')
+
+@app.get('/sort')
+
+def sort_patient(sort_by : str = Query(..., description='Feature based sorting'),order : 
+    str=Query('asc',description='Sort in Ascending or Descending order')):
+    valid_fields = ['height', 'weight','bmi'] 
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail='Invalid field. Select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order. Select asc or desc.')
+    
+    data = load_data()
+
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
 
