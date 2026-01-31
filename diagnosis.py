17c8abe4914b4a1d7bc579bd4095d22ca85800d4
sanjaraT <sanjaratunola@gmail.com>
2026-01-31 13:08:59 +0600

    Created Computed  Fields

--- diagnosis.py
+++ diagnosis.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,Path,HTTPException,Query
 import json
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, computed_field
 from typing import Annotated, Literal
 
 app = FastAPI()
@@ -14,7 +14,25 @@ class Patient(BaseModel):
     height : Annotated[float, Field(...,gt=0, description='Enter Patient Height in meters')]
     weight : Annotated[float, Field(...,gt=0, description='Enter Patient Weight in kgs')]
 
-    
+
+@computed_field
+@property
+def bmi(self)->float:
+    bmi = round(self.weight/(self.height**2),2)
+    return bmi
+
+@computed_field
+@property
+def verdict(self)->str:
+    if self.bmi<18.5:
+        return 'Underweight'
+    elif self.bmi<25:
+        return 'Normal'
+    elif self.bmi<30:
+        return 'Normal'
+    else:
+        return 'Obese'
+
 def load_data():
     with open('patients.json','r') as f:
         data = json.load(f)
